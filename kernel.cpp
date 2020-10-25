ecb3dd1e6c8a55d0b770059ebda4aa70326c8eda
Norah AlGhamdi <NAlGhamdi1441@gmail.com>
2020-10-25 05:50:50 -0700

    create mykernel.iso

--- kernel.cpp
+++ kernel.cpp
@@ -1,7 +1,7 @@
 
 void printf(char* str)
 {
-    unsigned short* VideoMemory = (unsigned short*)0xb8000;
+    static unsigned short* VideoMemory = (unsigned short*)0xb8000;
     for(int i = 0; str[i] != '\0'; ++i)
         VideoMemory[i] = (VideoMemory[i] & 0xFF00) | str[i];
     
@@ -9,15 +9,15 @@ void printf(char* str)
 
 
 typedef void (*constructor)();
-extern "C" constructor* start_ctors;
-extern "C" constructor* end_ctors;
+extern "C" constructor start_ctors;
+extern "C" constructor end_ctors;
 extern "C" void callConstructors()
 {
-        for(constructor* i = &start_ctors; i != end_ctors; i++)
-            (*i)();
+    for(constructor* i = &start_ctors; i != &end_ctors; i++)
+        (*i)();
 }    
  
-extern "C" void kernelMain(void* multiboot_structure, unsigned int magicnumber)
+extern "C" void kernelMain(const void* multiboot_structure, unsigned int /*multiboot_magic*/)
 {
     printf("Hello World! --- http://www.AlgorithMan.de");
     
